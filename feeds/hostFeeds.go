d85c2b91ee8324d529e264cb97a4e4ef5e3ca2ab
kota-yata <kota@yatagai.com>
2021-03-28 20:31:16 +0900

    add bin file

--- feeds/hostFeeds.go
+++ feeds/hostFeeds.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -15,8 +16,11 @@ func SetHost(rssFeeds string) {
 		req.Header.Set("Content-Type", "application/rss+xml")
 		return
 	})
-	// portName := ":" + os.Getenv("PORT")
+	portName := os.Getenv("PORT")
+	if portName == "" {
+		portName = "3432"
+	}
 	fmt.Println("RSS feed has been published at http://localhost:3432")
-	err := http.ListenAndServe(":3432", nil)
+	err := http.ListenAndServe(":" + portName, nil)
 	ErrorHandling(err)
 }
