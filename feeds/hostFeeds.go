e2db6dcd05c9c657867a383997f1f83c78f3a8f1
kota-yata <kota@yatagai.com>
2021-03-28 22:30:03 +0900

    add cert challenge

--- feeds/hostFeeds.go
+++ feeds/hostFeeds.go
@@ -16,11 +16,19 @@ func SetHost(rssFeeds string) {
 		req.Header.Set("Content-Type", "application/rss+xml")
 		return
 	})
+	uploadCertChallenge()
 	portName := os.Getenv("PORT")
 	if portName == "" {
 		portName = "3432"
 	}
-	fmt.Println("RSS feed has been published at http://localhost:3432")
+	fmt.Println("RSS feed has been published at http://localhost:" + portName)
 	err := http.ListenAndServe(":" + portName, nil)
 	ErrorHandling(err)
+}
+
+func uploadCertChallenge() {
+	http.HandleFunc("/.well-known/acme-challenge/o7AhrXD3WFtRBrHpZdQEiJCVoEuSZ6nJcqKkgA6DwCo", func(writer http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(writer, "o7AhrXD3WFtRBrHpZdQEiJCVoEuSZ6nJcqKkgA6DwCo.cNJeOkhi1FI7f7y1QEqdrZvDa29r3dp3Fz7mQrtdr2E")
+		return
+	})
 }
